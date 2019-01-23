6e91071a5ef1f5cbd48e2e76aa6e14f7f68d073a
Rohit Khattar <rohitkh@byu,edu>
2019-01-22 21:27:31 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/initPhase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/initPhase.dir/main.cpp.o"
+  "initPhase.pdb"
+  "initPhase"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/initPhase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
